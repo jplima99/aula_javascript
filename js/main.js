4351fd0e833959c6ee96427b277cedfeb6cc67bf
João Pedro Silva Lima <jp.silvalima327@gmail.com>
2020-07-07 14:41:00 -0300

    at2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,45 @@
+var d = new Date();
+alert(d.getMinutes());
+
+
+/*let cont = 0 ;
+for (cont; cont<=5; cont++){
+    alert(cont);
+};
+
+
+/*var cont = 0;
+while(cont < 5) {
+    console.log(cont);
+    cont ++;
+};
+
+
+/*var idade = prompt("Qual sua idade?");
+var idade = 18;
+if(idade>=18) {
+    alert("Maior de idade")
+} else {
+    alert("Menor de idade")
+};
+
+
+/*var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"banana" , cor:"amarelo"}];
+console.log(frutas);
+alert(frutas[1].nome);
+
+var fruta = {nome:"maça" , cor:"vermelha"};
+console.log(fruta.nome);
+
+
+var lista = ["maça" , "pera" , "laranja"];
+lista.push("uva");
+//lista.pop();
+console.log(lista);
+console.log(lista.toString());
+console.log(lista.join(" | "));
+
+
 var  nome = "joão Pedro";
 var n1 = 5;
 var n2 = 3;
@@ -7,3 +49,4 @@ var frase = "Japão é o melhor time do mundo";
 console.log(nome);
 console.log(n1 * n2);
 console.log(frase.toLocaleLowerCase());
+*/
